e5d2a27904298ec556e50fcf8af413bd72a6b598
gabrielcarvalhotp <gabrieltp2004@gmail.com>
2026-05-05 20:29:41 -0300

    add theme files including color scheme, typography, and main theme setup

--- app/src/main/java/br/com/hellodev/moviestreaming/MainActivity.kt
+++ app/src/main/java/br/com/hellodev/moviestreaming/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.hellodev.moviestreaming.ui.theme.MovieStreamingTheme
+import br.com.hellodev.moviestreaming.presenter.theme.MovieStreamingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/br/com/hellodev/moviestreaming/presenter/theme/Theme.kt
@@ -0,0 +1,114 @@
+package br.com.hellodev.moviestreaming.presenter.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+private val LightColorScheme = MyColorScheme(
+    defaultColor = DefaultColor,
+    disabledDefaultColor = DisabledDefaultColor,
+    primaryBackgroundColor = PrimaryBackgroundColorLight,
+    secondaryBackgroundColor = SecondaryBackgroundColorLight,
+    backgroundSocialButtonColor = BackgroundSocialButtonColorLight,
+    textFieldBackgroundColor = TextFieldBackgroundColorLight,
+    alphaDefaultColor = AlphaDefaultColor,
+    topAppBarColor = TopAppBarColorLight,
+    dividerColor = DividerColorLight,
+    borderColor = BorderColorLight,
+    textColor = TextColorLight,
+    iconColor = IconColorLight,
+    secondaryButtonColor = SecondaryButtonColorLight,
+    secondaryButtonTextColor = SecondaryButtonTextColorLight,
+    switchInactiveBackgroundColor = SwitchInactiveBackgroundColorLight,
+    switchActiveBackgroundColor = SwitchInactiveBackgroundColorLight,
+    tabRowUnselectedTextColor = TabRowUnselectedTextColorLight,
+    successColor = SuccessColor,
+    alertColor = AlertColor,
+    warningColor = WarningColor,
+    infoColor = InfoColor,
+    disabledColor = DisabledColor,
+    greyscale900Color = Greyscale900Color,
+    greyscale800Color = Greyscale800Color,
+    greyscale700Color = Greyscale700Color,
+    greyscale600Color = Greyscale600Color,
+    greyscale500Color = Greyscale500Color,
+    greyscale400Color = Greyscale400Color,
+    greyscale300Color = Greyscale300Color,
+    greyscale200Color = Greyscale200Color,
+    greyscale100Color = Greyscale100Color,
+    greyscale50Color = Greyscale50Color,
+    whiteColor = WhiteColor,
+    blackColor = BlackColor,
+    transparentColor = TransparentColor,
+    spotColor = SpotColor,
+    ambientColor = AmbientColor
+)
+
+private val DarkColorScheme = MyColorScheme(
+    defaultColor = DefaultColor,
+    disabledDefaultColor = DisabledDefaultColor,
+    primaryBackgroundColor = PrimaryBackgroundColorDark,
+    secondaryBackgroundColor = SecondaryBackgroundColorDark,
+    backgroundSocialButtonColor = BackgroundSocialButtonColorDark,
+    textFieldBackgroundColor = TextFieldBackgroundColorDark,
+    alphaDefaultColor = AlphaDefaultColor,
+    topAppBarColor = TopAppBarColorDark,
+    dividerColor = DividerColorDark,
+    borderColor = BorderColorDark,
+    textColor = TextColorDark,
+    iconColor = IconColorDark,
+    secondaryButtonColor = SecondaryButtonColorDark,
+    secondaryButtonTextColor = SecondaryButtonTextColorDark,
+    switchInactiveBackgroundColor = SwitchInactiveBackgroundColorDark,
+    switchActiveBackgroundColor = SwitchInactiveBackgroundColorDark,
+    tabRowUnselectedTextColor = TabRowUnselectedTextColorDark,
+    successColor = SuccessColor,
+    alertColor = AlertColor,
+    warningColor = WarningColor,
+    infoColor = InfoColor,
+    disabledColor = DisabledColor,
+    greyscale900Color = Greyscale900Color,
+    greyscale800Color = Greyscale800Color,
+    greyscale700Color = Greyscale700Color,
+    greyscale600Color = Greyscale600Color,
+    greyscale500Color = Greyscale500Color,
+    greyscale400Color = Greyscale400Color,
+    greyscale300Color = Greyscale300Color,
+    greyscale200Color = Greyscale200Color,
+    greyscale100Color = Greyscale100Color,
+    greyscale50Color = Greyscale50Color,
+    whiteColor = WhiteColor,
+    blackColor = BlackColor,
+    transparentColor = TransparentColor,
+    spotColor = SpotColor,
+    ambientColor = AmbientColor
+)
+
+private val LocalColorScheme = compositionLocalOf { LightColorScheme }
+
+object MovieStreamingTheme {
+    val colorScheme: MyColorScheme
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColorScheme.current
+}
+
+@Composable
+fun MovieStreamingTheme(
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme by remember(isDarkTheme) {
+        mutableStateOf(if (isDarkTheme) DarkColorScheme else LightColorScheme)
+    }
+
+    CompositionLocalProvider(LocalColorScheme provides colorScheme) {
+        MaterialTheme(content = content)
+    }
+}
