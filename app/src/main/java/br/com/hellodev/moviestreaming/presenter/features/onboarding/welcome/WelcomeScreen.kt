9be1f95104cc17e9afa33d07b6a28625658a5ea5
gabrielcarvalhotp <gabrieltp2004@gmail.com>
2026-05-06 21:30:31 -0300

    add welcome slides and slide indicator components with localization

--- app/src/main/java/br/com/hellodev/moviestreaming/presenter/features/onboarding/welcome/WelcomeScreen.kt
+++ app/src/main/java/br/com/hellodev/moviestreaming/presenter/features/onboarding/welcome/WelcomeScreen.kt
@@ -3,14 +3,23 @@ package br.com.hellodev.moviestreaming.presenter.features.onboarding.welcome
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
 import br.com.hellodev.moviestreaming.R
+import br.com.hellodev.moviestreaming.presenter.components.slide.WelcomeSlideUI
 import br.com.hellodev.moviestreaming.presenter.theme.MovieStreamingTheme
 
 @Composable
@@ -20,25 +29,62 @@ fun WelcomeScreen() {
 
 @Composable
 fun WelcomeContent() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MovieStreamingTheme.colorScheme.primaryBackgroundColor)
-    ) {
-        Image(
-            painter = painterResource(R.drawable.placeholder_welcome),
-            contentDescription = null,
-            contentScale = ContentScale.Crop
+    val slideItems = listOf(
+        Pair(
+            first = stringResource(R.string.welcome_title_1),
+            second = stringResource(R.string.welcome_description_1),
+        ),
+        Pair(
+            first = stringResource(R.string.welcome_title_2),
+            second = stringResource(R.string.welcome_description_2),
+        ),
+        Pair(
+            first = stringResource(R.string.welcome_title_3),
+            second = stringResource(R.string.welcome_description_3),
         )
-
-        Image(
-            painter = painterResource(R.drawable.background_gradient),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-        )
+    )
+    val pagerState = rememberPagerState {
+        slideItems.size
     }
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        content = { paddingValues ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MovieStreamingTheme.colorScheme.primaryBackgroundColor)
+                    .paint(
+                        painter = painterResource(id = R.drawable.placeholder_welcome),
+                        contentScale = ContentScale.Crop
+                    )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.background_gradient),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.BottomCenter),
+                    contentScale = ContentScale.Crop
+                )
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(paddingValues)
+                        .padding(bottom = 24.dp)
+                ) {
+                    WelcomeSlideUI(
+                        modifier = Modifier
+                            .weight(1f),
+                        slideItems = slideItems,
+                        pagerState = pagerState
+                    )
+                }
+            }
+        }
+    )
 }
 
 @PreviewLightDark
