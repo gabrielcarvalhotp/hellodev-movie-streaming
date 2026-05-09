ac476bf0bf068ef181992e439bb2149458f4fd9d
gabrielcarvalhotp <gabrieltp2004@gmail.com>
2026-05-09 15:30:03 -0300

    add Kotlin serialization and ViewModel dependencies in build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -50,9 +51,17 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.viewmodel.compose)
+    implementation(libs.navigation.compose)
+
+    // Koin
+    implementation(libs.koin.compose)
 
     // Lottie
     implementation(libs.lottie.compose)
+
+    // Serialization
+    implementation(libs.serialization.json)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
