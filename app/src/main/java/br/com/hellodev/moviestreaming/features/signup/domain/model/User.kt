2588f1585fc49a812cff9a0c556be00baa56d129
gabrielcarvalhotp <gabrieltp2004@gmail.com>
2026-05-10 13:11:00 -0300

    add User model in feature signup

---
+++ app/src/main/java/br/com/hellodev/moviestreaming/features/signup/domain/model/User.kt
@@ -0,0 +1,12 @@
+package br.com.hellodev.moviestreaming.features.signup.domain.model
+
+data class User(
+    val id: String? = null,
+    val name: String? = null,
+    val surname: String? = null,
+    val email: String? = null,
+    val photo: String? = null,
+    val phone: String? = null,
+    val genre: String? = null,
+    val country: String? = null
+)
